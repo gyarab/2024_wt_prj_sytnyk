e0d94386cf0b7d313c9843547add06b3e790f075
Veronika Sytnyk <veronika.sytnyk.s@gyarab.cz>
2025-02-14 10:15:56 +0100

    changes

---
+++ exchange.py
@@ -0,0 +1,20 @@
+import httpx
+
+print("Online prevodnik men dle cnb")
+
+url = 'https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt;jsessionid=5EBE7E79ED83B35877829238E5BBB94D?date=13.02.2025'
+
+r = httpx.get('url')
+
+#print(r.text)
+
+in_array = r.text.split("\n")
+#print(in_array)
+
+for line in r.text:
+    if "|EUR|" in line:
+        print(line)
+
+castka = input("Zadej castku v CZK: ")
+result = int(castka) * 25
+print(f"To je v EUR: {result}")
