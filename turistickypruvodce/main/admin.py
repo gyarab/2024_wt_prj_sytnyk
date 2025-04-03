810a601ac52fda2f165f3fbb7d69786398d19582
nimayo <veronika.sytnyk.s@gyarab.cz>
2025-04-03 22:23:57 +0200

    changes

--- turistickypruvodce/main/admin.py
+++ turistickypruvodce/main/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from .models import Sight
+from .models import State
+from .models import Category
 
 class SightAdmin(admin.ModelAdmin):
     list_display = ["id", "name", "location", "description"]
 
+class StateAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+
 # Register your models here.
 admin.site.register(Sight, SightAdmin)
+admin.site.register(State, StateAdmin)
+admin.site.register(Category, CategoryAdmin)
