6719ca8a776ca3d35a2cb5bd8355ce1dbb9b845e
nimayo <veronika.sytnyk.s@gyarab.cz>
2025-04-05 20:08:00 +0200

    changes

--- turistickypruvodce/main/models.py
+++ turistickypruvodce/main/models.py
@@ -6,6 +6,8 @@ class Sight(models.Model):
     description = models.TextField(blank=True, default="")
     link = models.CharField(max_length=300, blank=True, default="")
     #director = models.ForeignKey("Director", on_delete=models.SET_NULL, null=True)
+    state = models.ForeignKey("State", on_delete=models.SET_NULL, null=True)
+    categorie = models.ForeignKey("Categorie", on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f"Sight <{self.id}> {self.name} ({self.location})"
@@ -18,6 +20,7 @@ class State(models.Model):
 
 class Categorie(models.Model):
     name = models.CharField(max_length=50)
+    sight = models.ForeignKey("Sight", on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f"Categorie <{self.id}> {self.name}"
