598402832d3e440623c56539145dabe126aaa4f5
Veronika Sytnyk <veronika.sytnyk.s@gyarab.cz>
2025-05-02 10:39:26 +0200

    commit

--- turistickypruvodce/main/views.py
+++ turistickypruvodce/main/views.py
@@ -2,11 +2,20 @@ from django.shortcuts import render
 from main.models import Sight
 
 def get_homepage(request):
-    context = {
-        "svatek": "nika",
         # SELECT *from.Sights LIMIT 10
-        "sights": Sight.objects.all().order_by('name')[:10]
-    } 
+        "sight": Sight.objects.all().order_by('name')
+    
+    return render(
+        request, "main/homepage.html", context
+    )
+
+    if request.GET.get("search"):
+        sights = sights.filter(name__icontant=request.GET.get("search"))
+
+    context = {
+        "sights": sights,
+    }
+
     return render(
         request, "main/homepage.html", context
     )
