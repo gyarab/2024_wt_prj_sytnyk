fd0026ee4302a66af0edd9f5faa14e3652225bc9
Veronika Sytnyk <veronika.sytnyk.s@gyarab.cz>
2025-05-16 11:18:57 +0200

    change

--- turistickypruvodce/main/views.py
+++ turistickypruvodce/main/views.py
@@ -1,21 +1,33 @@
 from django.shortcuts import render
-from main.models import Sight
+from main.models import Sight, Categorie
+#from faker import Faker
 
 def get_homepage(request):
-        # SELECT *from.Sights LIMIT 10
-        "sight": Sight.objects.all().order_by('name')
+    # SELECT *from.Sights LIMIT 10
+    sights = Sight.objects.all().order_by('name')
+
+    search = request.GET.get('search')
+    if search:
+        sights = sights.filter(name__icontains=search) | sights.filter(description__icontains=search)
+
+    # filter by category if query parameter category is present
+    categorie = request.GET.get('categorie')
+    if categorie:
+        sights = sights.filter(categories__name=categorie)
     
-    return render(
-        request, "main/homepage.html", context
-    )
-
-    if request.GET.get("search"):
-        sights = sights.filter(name__icontant=request.GET.get("search"))
-
     context = {
         "sights": sights,
+        "categories": Categorie.objects.all().order_by('name'),
     }
 
+    #def random_person(request):
+     #   context = {
+        # use faker to generate random data
+     #   "name": Faker().name(),
+     #   "email": Faker().email(),
+     #   "phone": Faker().phone_number(),
+    #}
+
     return render(
-        request, "main/homepage.html", context
+        request, "main/random.html", context
     )
